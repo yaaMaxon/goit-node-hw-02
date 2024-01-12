45e03985ebc72cb869e908defb0d912b6c09ceb8
Maksym <maks.sansei@gmail.com>
2024-01-12 10:01:15 +0200

    fixed some errors in app

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -60,8 +60,6 @@ const getCurrent = async(req, res) => {
     const { email, subscription } = req.user;
 
     res.json({
-        status: "success",
-        code: 200,
         user: {
             email,
             subscription
